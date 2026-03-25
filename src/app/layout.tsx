8fdc9ca62613da8e9128d716ee463a6f61893bc5
Zakiul Fahmi Jailani, MSc <30364443+zakiulfahmijailani@users.noreply.github.com>
2026-03-25 07:33:50 +0700

    feat: update title and metadata for Rumaisha Qonita

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Aqiqah Izhan Faqqihhu Fiddin",
+  title: "Aqiqah Rumaisha Qonita",
   description:
-    "Undangan Syukuran Aqiqah putra Bapak Zakiul Fahmi Jailani & Ibu Kemala Putri Ayunda",
+    "Undangan Syukuran Aqiqah putri Bapak Tri Rachmat Riski & Ibu Upita Anggunsuri",
 };
 
 export default function RootLayout({
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="id" className="scroll-smooth">
       <head>
-        <meta name="theme-color" content="#fafaeb" />
+        <meta name="theme-color" content="#fff0f5" />
       </head>
       <body className="min-h-screen locked">{children}</body>
     </html>
